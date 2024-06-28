b5ed55fc6dc7dda95a5da5427f7ba3b3b04628fe
Serhii Zubenko <sergzubenko@gmail.com>
2024-06-28 21:00:07 +0300

    initial

---
+++ src/main/java/ua/dp/java/cicdact/CicdactApplication.java
@@ -0,0 +1,13 @@
+package ua.dp.java.cicdact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdactApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/dp/java/cicdact/CicdactApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.dp.java.cicdact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
